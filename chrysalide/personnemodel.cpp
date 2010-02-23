20e859495a37d29ec403ba651c66f379465f771a
antonyrey <antonyrey@gmail.com>
2010-02-23 15:34:56 +0100

    classes personnel et patient

--- chrysalide/personnemodel.cpp
+++ chrysalide/personnemodel.cpp
@@ -6,3 +6,17 @@ personneModel::personneModel(long pId, QString pNom, QString pPrenom)
     nom = pNom;
     prenom = pPrenom;
 }
+
+void personneModel::majProfil(){
+
+    QSqlQuery maj("UPDATE PERSONNE SET nom = '"+nom+"', prenom = '"+prenom+"' WHERE id = "+QString::number(id));
+    maj.exec();
+
+}
+
+void personneModel::supprimer(){
+
+    QSqlQuery supprimer("DELETE FROM PERSONNE WHERE id = "+QString::number(id));
+    supprimer.exec();
+
+}
