6604e3bfd0e27f3b62c2a17d27d646d4bf933820
gthom <gthomassingap@gmail.com>
2010-02-23 13:01:13 +0100

    maj script de creation

--- creBaseChrysalide.sql
+++ creBaseChrysalide.sql
@@ -1,6 +1,13 @@
+create table TYPETABLE
+{
+   	numero integer not null primary key,
+	libelle varchar(50) not null
+
+}
 create table TABLEAMANGER
 (
 	numero integer not null primary key,
 	capacite integer not null,
-	typeTable integer references TYPETABLE(numero)
+	typeTable integer not null references TYPETABLE(numero)
+
 );
