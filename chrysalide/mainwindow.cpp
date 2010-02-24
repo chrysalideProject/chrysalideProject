6a9534c68f71db0190da78a8dba90c6f75817064
gthom <gthomassingap@gmail.com>
2010-02-24 09:39:42 +0100

    création des slots correspondant aux actions du menu

--- chrysalide/mainwindow.cpp
+++ chrysalide/mainwindow.cpp
@@ -4,6 +4,7 @@
 #include <QMessageBox>
 #include <QSqlDatabase>
 #include <QCloseEvent>
+#include <QDebug>
 
 MainWindow::MainWindow(QSqlDatabase* pDatabase, QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindowClass)
@@ -19,6 +20,8 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_action_Ouvrir_triggered()
 {
+    qDebug()<<"void MainWindow::on_action_Ouvrir_triggered()";
+    //ouverture de la base sqlite
     QFileDialog dialogOuvertureBase(this);
     dialogOuvertureBase.setFilter("*.db");
   if(dialogOuvertureBase.exec())
@@ -35,15 +38,75 @@ void MainWindow::on_action_Ouvrir_triggered()
 
 void MainWindow::on_action_Quitter_triggered()
 {
+    qDebug()<<"void MainWindow::on_action_Quitter_triggered()";
+    //choix de l'option quitter du menu
+    qDebug()<<"void MainWindow::on_action_Quitter_triggered()";
   close();
 }
 
 void MainWindow::onCloseEvent(QCloseEvent* event)
 {
+    //demande de fermeture de la part de l'utilisateur
+    qDebug()<<"void MainWindow::onCloseEvent(QCloseEvent* event)";
     if (QMessageBox::warning(this,this->windowTitle()+" Quitter?","Etes-vous sûr de vouloir quitter l'application?",QMessageBox::Yes|QMessageBox::No,QMessageBox::No)==QMessageBox::Yes)
     {
         event->accept();
     }
     else
         event->ignore();
+}
+
+void MainWindow::on_actionArriv_e_triggered()
+{
+   qDebug()<<"void MainWindow::on_actionArriv_e_triggered()";
+   //arrivée d'une personne concernée par l'application
+}
+
+void MainWindow::on_actionD_part_triggered()
+{
+    qDebug("void MainWindow::on_actionD_part_triggered()");
+    //départ d'une personne quelle qu'elle soit
+}
+
+void MainWindow::on_actionRentrer_les_Tables_triggered()
+{
+    qDebug()<<"void MainWindow::on_actionRentrer_les_Tables_triggered()";
+    //configuration initiale on rentre les tables constituant la salle de restauration
+}
+
+void MainWindow::on_action_Nouveau_triggered()
+{
+        qDebug()<<"void MainWindow::on_action_Nouveau_triggered()";
+        //nouvelle base de donnée
+        //création de la base et exécution du script sql de création des tables sans jeu d'essais
+}
+
+void MainWindow::on_action_Pr_paration_des_tables_triggered()
+{
+        qDebug()<<"void MainWindow::on_action_Pr_paration_des_tables_triggered()";
+        //le gros du taf: oraganisation du repas
+}
+
+void MainWindow::on_action_gestion_des_affinit_s_triggered()
+{
+        qDebug()<<"void MainWindow::on_action_gestion_des_affinit_s_triggered()";
+        //gestion des incidents et des affinités entre patients
+}
+
+void MainWindow::on_action_Propos_triggered()
+{
+        qDebug()<<"void MainWindow::on_action_Propos_triggered()";
+        //là on donne des infos sur les auteurs
+}
+
+void MainWindow::on_action_Manuel_triggered()
+{
+        qDebug()<<"void MainWindow::on_action_Manuel_triggered()";
+        //activation du manuel de l'utilisateur
+}
+
+void MainWindow::on_action_Sauvegarde_triggered()
+{
+    qDebug()<<"void MainWindow::on_action_Sauvegarde_triggered()";
+    //sauvegarde de la base sur un support amovible
 }
--- chrysalide/mainwindow.h
+++ chrysalide/mainwindow.h
@@ -22,6 +22,15 @@ private:
     QSqlDatabase* database;
 
 private slots:
+    void on_action_Sauvegarde_triggered();
+    void on_action_Manuel_triggered();
+    void on_action_Propos_triggered();
+    void on_action_gestion_des_affinit_s_triggered();
+    void on_action_Pr_paration_des_tables_triggered();
+    void on_action_Nouveau_triggered();
+    void on_actionRentrer_les_Tables_triggered();
+    void on_actionD_part_triggered();
+    void on_actionArriv_e_triggered();
     void on_action_Quitter_triggered();
     void on_action_Ouvrir_triggered();
     void onCloseEvent(QCloseEvent* event);
